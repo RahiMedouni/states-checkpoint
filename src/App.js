16fc598d88eea8be2d74c890a4e56c79ecc166a6
RahiMedouni <rahimedouni@gmail.com>
2022-01-12 18:49:35 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import Counter from "./Counter";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    fullName: "Rahim Mdn",
+    bio: "I'm a web developer, this my experiences through the years...",
+    imgSrc: "/imagevictor.jpg",
+    profession: "Web Developer",
+    show: false,
+  };
+
+  handleClick = () => {
+    this.setState({
+      show: !this.state.show,
+    });
+  };
+
+  render() {
+    return (
+      <div className="App">
+        {this.state.show ? (
+          <>
+            <h1>{this.state.fullName}</h1>
+            <h1>{this.state.bio}</h1>
+            <img src={this.state.imgSrc} alt="failed" />
+            <h1>{this.state.profession}</h1>
+          </>
+        ) : (
+          <h1>Show the profile</h1>
+        )}
+        <button onClick={this.handleClick}>
+          {!this.state.show ? "show" : "hide"}
+        </button>
+        <>
+          <h1>Show the count</h1>
+          <Counter />
+        </>
+      </div>
+    );
+  }
 }
 
 export default App;
